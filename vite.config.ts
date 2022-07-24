dfb5440fb5f8c283fb44c605b95fd74bd78a0559
Steven Conaway <steven.conaway@icloud.com>
2022-07-23 23:31:01 -0400

    use code splitting more

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,37 @@
 import react from '@vitejs/plugin-react';
+
 import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
+import { dependencies } from './package.json';
+
+// Packages we want in the vendor aka the deps needed in the entire app.
+const globalVendorPackages = [
+  'react',
+  'react-dom',
+  // 'react-router-dom',
+  '@geist-ui/core',
+];
+
+function renderChunks(deps: Record<string, string>) {
+  let chunks = {};
+  Object.keys(deps).forEach((key) => {
+    if (globalVendorPackages.includes(key)) return;
+    chunks[key] = [key];
+  });
+  return chunks;
+}
+
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: globalVendorPackages,
+          ...renderChunks(dependencies),
+        },
+      },
+    },
+  },
 });
