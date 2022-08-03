496f6602ee71485f4edc67b11b3f06f82cace981
Steven Conaway <steven.conaway@icloud.com>
2022-08-03 14:34:12 -0600

    ok don't chunk at all

--- vite.config.ts
+++ vite.config.ts
@@ -1,55 +1,18 @@
-import { defineConfig, UserConfig } from 'vite';
+import { defineConfig } from 'vite';
 
 import react from '@vitejs/plugin-react';
 import Pages from 'vite-plugin-pages';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
-import { dependencies } from './package.json';
+import { visualizer } from 'rollup-plugin-visualizer';
 
-// Packages we want in the vendor aka the deps needed in the entire app.
-const globalVendorPackages = [
-  'react',
-  'react-dom',
-  // 'react-router-dom',
-  '@geist-ui/core',
-];
-
-function renderChunks(deps: Record<string, string>) {
-  let chunks = {};
-  Object.keys(deps).forEach((key) => {
-    if (globalVendorPackages.includes(key)) return;
-    chunks[key] = [key];
-  });
-  return chunks;
-}
-
-export default defineConfig(({ mode }) => {
-  let baseConfig: UserConfig = {
-    plugins: [react(), Pages(), tsconfigPaths()],
-    build: {
-      sourcemap: true,
-    },
-    css: {
-      devSourcemap: true,
-    },
-    envPrefix: 'PUBLIC_',
-  };
-
-  // in prod, chunk; in dev, don't
-  if (mode === 'development') return baseConfig;
-  else
-    return {
-      ...baseConfig,
-      build: {
-        ...baseConfig.build,
-        rollupOptions: {
-          output: {
-            manualChunks: {
-              vendor: globalVendorPackages,
-              ...renderChunks(dependencies),
-            },
-          },
-        },
-      },
-    };
+export default defineConfig({
+  plugins: [react(), Pages(), tsconfigPaths(), visualizer()],
+  build: {
+    sourcemap: true,
+  },
+  css: {
+    devSourcemap: true,
+  },
+  envPrefix: 'PUBLIC_',
 });
